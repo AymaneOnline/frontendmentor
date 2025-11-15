21824fd08cf796eb79b00e98328f08658af9df31
AymaneOnline <aymaneprowork@gmail.com>
2025-11-15 11:30:42 +0100

    adding homepage to package.json

--- rest-countries-api-with-color-theme-switcher-master/vite.config.js
+++ rest-countries-api-with-color-theme-switcher-master/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/AymaneOnline.github.io/frontendmentor/rest-countries-api-with-color-theme-switcher-master/', // GitHub Pages path
+  base: '/frontendmentor/rest-countries-api-with-color-theme-switcher-master/', // GitHub Pages path
 })

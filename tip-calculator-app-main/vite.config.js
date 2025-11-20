376c3106905dc0fdb20ca15ddd2e59b67df201c0
AymaneOnline <aymaneprowork@gmail.com>
2025-11-20 10:13:13 +0100

    fixing the deploy command

--- tip-calculator-app-main/vite.config.js
+++ tip-calculator-app-main/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // Serve existing images folder as public assets without moving files
   publicDir: 'images',
+  base: '/frontendmentor/tip-calculator-app-main/',
   test: {
     environment: 'jsdom',
     globals: true,
